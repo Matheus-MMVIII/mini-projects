89b4f85b732a8bd041ee132681bedbb1bcc95883
Matheus-MMVIII <matheusviturino8@gmail.com>
2026-09-22 12:55:19 -0300

    Rework logic in for to aceppt odds numbers.

--- src/main/java/com/image_resizer/Main.java
+++ src/main/java/com/image_resizer/Main.java
@@ -15,6 +15,7 @@ public class Main {
     }
 
     public static void reduce(File file, int numReduce) throws IOException {
+        System.out.println("Is "+numReduce+" time.");
         BufferedImage bufferedImage = ImageIO.read(file);
 
         int width = bufferedImage.getWidth();
@@ -23,35 +24,51 @@ public class Main {
         if (width <= 1 && height <= 1) {
             return;
         }
-        int[] rgbs = new int[width * height];
 
+        int[] rgbs = new int[width * height];
         bufferedImage.getRGB(0, 0, width, height, rgbs, 0, width);
 
         int newWidth = (width + 1) / 2;
         int newHeight = (height + 1) / 2;
 
-        BufferedImage newImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+        BufferedImage newImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
 
-        for (int y = 0; y < height; y+=2) {
-            for (int x = 0; x < width; x+=2) {
-                Color c1 = new Color(rgbs[y * width + x], true);
-                Color c2 = new Color(rgbs[y * width + x + 1], true);
-                Color c3 = new Color(rgbs[(y + 1) * width + x], true);
-                Color c4 = new Color(rgbs[(y + 1) * width + x + 1], true);
+        for (int newY = 0; newY < newHeight; newY++) {
+            for (int newX = 0; newX < newWidth; newX++) {
 
-                int r = (c1.getRed() + c2.getRed() + c3.getRed() + c4.getRed()) / 4;
-                int g = (c1.getGreen() + c2.getGreen() + c3.getGreen() + c4.getGreen()) / 4;
-                int b = (c1.getBlue() + c2.getBlue() + c3.getBlue() + c4.getBlue()) / 4;
-                int a = (c1.getAlpha() + c2.getAlpha() + c3.getAlpha() + c4.getAlpha()) / 4;
+                int startX = newX * 2;
+                int startY = newY * 2;
 
-                Color finalRgb = new Color(r, g, b, a);
+                int endX = Math.min(startX + 2, width);
+                int endY = Math.min(startY + 2, height);
 
-                newImage.setRGB(x / 2, y / 2, finalRgb.getRGB());
+                int r = 0;
+                int g = 0;
+                int b = 0;
+                int a = 0;
+                int count = 0;
+
+                for (int y = startY; y < endY; y++) {
+                    for (int x = startX; x < endX; x++) {
+                        Color color = new Color(rgbs[y * width + x], true);
+
+                        r += color.getRed();
+                        g += color.getGreen();
+                        b += color.getBlue();
+                        a += color.getAlpha();
+
+                        count++;
+                    }
+                }
+
+                Color finalColor = new Color(r / count, g / count, b / count, a / count);
+
+                newImage.setRGB(newX, newY, finalColor.getRGB());
             }
         }
 
-        File newFile = new File(PATH+numReduce+"-reduce.png");
+        File newFile = new File(PATH + numReduce + "-reduce.png");
         ImageIO.write(newImage, "png", newFile);
-        reduce(newFile, ++numReduce);
+        reduce(newFile, numReduce + 1);
     }
 }
